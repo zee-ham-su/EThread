45bfb587446e744a8042b85bddac0799a99b0677
Hamza Sufian <hsufiian@yahoo.com>
2024-03-25 11:51:08 +0000

    Add server.js file for TechThread API

---
+++ Thread/Api/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+const checkOutRoutes = require('./routes/checkOutRoutes');
+const path = require('path');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const connectDB = require('./mondb.js');
+connectDB();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
+app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
+
+app.use('/api/products', productRoutes);
+app.use('/api/carts', cartRoutes);
+app.use('/api/reviews', reviewRoutes);
+app.use('/api/checkouts', checkOutRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(3000, () => {
+  console.log('TechThread app listening on port 3000!');
+});
