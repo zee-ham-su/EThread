f820960c85c9b94652de959df0ad7c24913749f1
Hamza Sufian <hsufiian@yahoo.com>
2024-03-28 12:44:56 +0000

    Add Cart and Product models to cartController.test.js

--- Thread/Api/tests/test.controllers/cartController.test.js
+++ Thread/Api/tests/test.controllers/cartController.test.js
@@ -1,5 +1,7 @@
 const CartController = require('../../controllers/cartController');
 const mongoose = require('mongoose');
+const Cart = require('../../models/cart');
+const Product = require('../../models/product');
 
 // Set up MongoDB connection before running tests
 beforeAll(async () => {
@@ -7,7 +9,7 @@ beforeAll(async () => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-});
+}, 10000);
 
 describe('Cart Controller', () => {
   let req, res, next;
@@ -25,48 +27,6 @@ describe('Cart Controller', () => {
     it('should add items to cart', async () => {
       req.body = {
         items: [{ product: 'mockProductId', quantity: 2 }],
-      };
-
-      await CartController.addToCart(req, res);
-
-      expect(res.status).toHaveBeenCalledWith(201);
-      expect(res.json).toHaveBeenCalledWith(expect.objectContaining({ items: expect.any(Array) }));
-    });
-  });
-});
-
-// Close MongoDB connection after all tests are done
-afterAll(async () => {
-    await mongoose.connection.close();
-  });const CartController = require('../../controllers/cartController');
-const mongoose = require('mongoose');
-const Cart = require('../../models/cartModel');
-const Product = require('../../models/productModel');
-
-// Set up MongoDB connection before running tests
-beforeAll(async () => {
-  await mongoose.connect('mongodb+srv://hamzasufian2014:DafBrscdZELfxbvc@cluster0.nmmyd8v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-});
-
-describe('Cart Controller', () => {
-  let req, res, next;
-
-  beforeEach(() => {
-    req = {};
-    res = {
-      json: jest.fn(),
-      status: jest.fn(() => res),
-    };
-    next = jest.fn();
-  });
-
-  describe('addToCart', () => {
-    it('should add items to cart', async () => {
-      req.body = {
-        items: [{ productId: 'mockProductId', quantity: 2 }],
       };
 
       await CartController.addToCart(req, res);
